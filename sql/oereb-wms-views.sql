f3de8fa66f5860217d0772d8e8323ed0f33325df
Andreas Neumann <a.neumann@carto.net>
2019-07-11 15:49:27 +0200

    first view nutzungsplanung.grundnutzung

--- sql/oereb-wms-views.sql
+++ sql/oereb-wms-views.sql
@@ -1,8 +1,30 @@
-SELECT geom.t_id, geom.t_type, geom.t_ili_tid, geom.flaeche_lv95, geom.rechtsstatus as rechtsstatus_geom,
-       geom.publiziertab as publiziertab_geom, geom.metadatengeobasisdaten, geom.eigentumsbeschraenkung,
-       eigbe.aussage, eigbe.thema, eigbe.subthema, eigbe.weiteresthema,
-       eigbe.artcode, eigbe.artcodeliste, eigbe.rechtsstatus as rechtsstatus_eigentumsbeschraenkung,
-       eigbe.publiziertab as publiziertab_eigentumsbeschraenkung
-FROM live.oerbkrmfr_v1_1transferstruktur_geometrie geom
-left join live.oerbkrmfr_v1_1transferstruktur_eigentumsbeschraenkung eigbe on geom.eigentumsbeschraenkung = eigbe.t_id
-left join live.oerbkrmvs_v1_1vorschriften_amt zust on geom.zustaendigestelle = zust.t_id;
+-- Nutzungsplanung_Grundnutzung 
+CREATE OR replace VIEW live.vw_oerebwms__nutzungsplanung_grundnutzung 
+AS 
+  SELECT geom.t_id, 
+         geom.t_type, 
+         geom.t_ili_tid, 
+         geom.flaeche_lv95, 
+         geom.rechtsstatus  AS rechtsstatus_geom, 
+         zustgeom.aname     AS zustaendigestelle_geom, 
+         zustgeom.amtimweb  AS amtimweb_geom, 
+         geom.publiziertab  AS publiziertab_geom, 
+         geom.metadatengeobasisdaten, 
+         geom.eigentumsbeschraenkung, 
+         eigbe.aussage, 
+         eigbe.thema, 
+         eigbe.subthema, 
+         eigbe.weiteresthema, 
+         eigbe.artcode, 
+         eigbe.artcodeliste, 
+         eigbe.rechtsstatus AS rechtsstatus_eigentumsbeschraenkung, 
+         eigbe.publiziertab AS publiziertab_eigentumsbeschraenkung 
+  FROM   live.oerbkrmfr_v1_1transferstruktur_geometrie geom 
+         left join live.oerbkrmfr_v1_1transferstruktur_eigentumsbeschraenkung eigbe 
+                ON geom.eigentumsbeschraenkung = eigbe.t_id 
+         left join live.oerbkrmvs_v1_1vorschriften_amt zustgeom 
+                ON geom.zustaendigestelle = zustgeom.t_id 
+         left join live.oerbkrmvs_v1_1vorschriften_amt zusteigbe 
+                ON eigbe.zustaendigestelle = zusteigbe.t_id 
+  WHERE  eigbe.subthema = 'ch.so.NutzungsplanungGrundnutzung' 
+         AND geom.flaeche_lv95 IS NOT NULL; 
