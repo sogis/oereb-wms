699b731a7f7f064d3eef454b5ad452ff02cd004e
Andreas Neumann <a.neumann@carto.net>
2019-08-06 15:49:33 +0200

    first gradle script for xtf-import

---
+++ gradle/xtfimport.sh
@@ -0,0 +1,28 @@
+echo "Importing xtf-file Nutzungsplanung"
+
+export PGHOST=db
+export PGPORT=5432
+export PGDATABASE=oereb
+export PGUSER=gretl
+export PGPASSWORD=gretl
+
+sleep 10
+
+: '
+echo "Waiting for PostgreSQL to start.."
+while true; do
+    pg_isready --timeout=2
+    if [ $? -eq 0 ]; then
+        echo "The database is ready."
+        break
+    fi
+    sleep 2
+done
+'
+
+# Have some extra sleep
+sleep 3
+
+# Run the import
+
+gradle --no-daemon
