01b7349561ac1ae6756866c778ba6ffeb32fd9cf
Andreas Neumann <a.neumann@carto.net>
2019-12-20 17:04:09 +0100

    adding test script to check status codes

---
+++ tests/test-wms.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#script for testing the status code of a GetCapabilities request. Should normally output status code 200
+STATUS_CODE_OEREB=`/usr/bin/curl -LI -o /dev/null -w "%{http_code}\n" -s "http://localhost/wms/oereb?SERVICE=WMS&REQUEST=GetCapabilities"`
+echo "HTTP STATUS-CODE-OEREB=$STATUS_CODE_OEREB"
+
+STATUS_CODE_OEREBSYMBOLS=`/usr/bin/curl -LI -o /dev/null -w "%{http_code}\n" -s "http://localhost/wms/oereb-symbols?SERVICE=WMS&REQUEST=GetCapabilities"`
+echo "HTTP STATUS-CODE-OEREB-SYMBOLS=$STATUS_CODE_OEREBSYMBOLS"
+
+if [ $STATUS_CODE_OEREB = 200 ] && [ $STATUS_CODE_OEREBSYMBOLS = 200 ]
+then
+  exit 0
+else
+  exit 1
+fi
