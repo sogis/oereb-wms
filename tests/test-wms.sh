17e54f92408d059c61f3142c9303f0d78fea0a0d
Andreas Neumann <a.neumann@carto.net>
2020-03-20 13:50:44 +0100

    Build and test image on Travis CI instead of Docker Hub (#26)

---
+++ tests/test-wms.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#script for testing the status code of a GetCapabilities request. Should normally output status code 200
+
+#give qgis-server and pg a bit of time to properly initiate
+echo "wait 20 seconds to allow PG and QGIS server to properly initiate"
+sleep 20
+
+STATUS_CODE_OEREB=`/usr/bin/curl -LI -o /dev/null -w "%{http_code}\n" -s "http://qgis-server/wms/oereb?SERVICE=WMS&REQUEST=GetCapabilities"`
+echo "HTTP STATUS-CODE-OEREB=$STATUS_CODE_OEREB"
+
+STATUS_CODE_OEREBSYMBOLS=`/usr/bin/curl -LI -o /dev/null -w "%{http_code}\n" -s "http://qgis-server/wms/oereb-symbols?SERVICE=WMS&REQUEST=GetCapabilities"`
+echo "HTTP STATUS-CODE-OEREB-SYMBOLS=$STATUS_CODE_OEREBSYMBOLS"
+
+if [ $STATUS_CODE_OEREB = 200 ] && [ $STATUS_CODE_OEREBSYMBOLS = 200 ]
+then
+  exit 0
+else
+  exit 1
+fi
