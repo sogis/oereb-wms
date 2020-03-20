2ef6b6af4ae118b3cde014c9657668f5bc49fa83
andreasneumann <a.neumann@carto.net>
2020-03-20 11:42:48 +0100

    Finally get testing to work

--- tests/test-wms.sh
+++ tests/test-wms.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 #script for testing the status code of a GetCapabilities request. Should normally output status code 200
-STATUS_CODE_OEREB=`/usr/bin/curl -LI -o /dev/null -w "%{http_code}\n" -s "http://localhost/wms/oereb?SERVICE=WMS&REQUEST=GetCapabilities"`
+
+#give qgis-server and pg a bit of time to properly initiate
+echo "wait 20 seconds to allow PG and QGIS server to properly initiate"
+sleep 20
+
+STATUS_CODE_OEREB=`/usr/bin/curl -LI -o /dev/null -w "%{http_code}\n" -s "http://qgis-server/wms/oereb?SERVICE=WMS&REQUEST=GetCapabilities"`
 echo "HTTP STATUS-CODE-OEREB=$STATUS_CODE_OEREB"
 
-STATUS_CODE_OEREBSYMBOLS=`/usr/bin/curl -LI -o /dev/null -w "%{http_code}\n" -s "http://localhost/wms/oereb-symbols?SERVICE=WMS&REQUEST=GetCapabilities"`
+STATUS_CODE_OEREBSYMBOLS=`/usr/bin/curl -LI -o /dev/null -w "%{http_code}\n" -s "http://qgis-server/wms/oereb-symbols?SERVICE=WMS&REQUEST=GetCapabilities"`
 echo "HTTP STATUS-CODE-OEREB-SYMBOLS=$STATUS_CODE_OEREBSYMBOLS"
 
 if [ $STATUS_CODE_OEREB = 200 ] && [ $STATUS_CODE_OEREBSYMBOLS = 200 ]
